dc495915c226edda1e62ca1e141b1d2f5226e0b6
nikas <nikasjason1@gmail.com>
2024-10-09 19:44:42 +0300

    Fix not terminating when reaching EOF

--- Project1/ask2/ask2.c
+++ Project1/ask2/ask2.c
@@ -21,11 +21,9 @@ typedef struct worker {
     long long int value_to_process;
 } worker_t;
 
-static worker_t *workers;
-
-int check_terminated_workers(int N);
-int check_available_worker(int N);
-int create_workers(int N);
+int check_terminated_workers(const worker_t *workers, int N);
+int check_available_worker(const worker_t *workers, int N);
+int create_workers(worker_t **workers, int N);
 void *run_worker(void *arg);
 int is_prime(long long int N);
 
@@ -37,61 +35,33 @@ int main(const int argc, char *argv[]) {
     const int N = atoi(argv[1]);
     long long int input;
 
-    create_workers(N);
+    worker_t *workers;
+    create_workers(&workers, N);
 
     while (1) {
-        scanf("%lld", &input);
-        if (input == -1)
+        const int read_result = scanf("%lld", &input);
+        if (read_result == EOF)
             break;
 
         // wait until you find an available worker
-        int available_worker = check_available_worker(N);
+        int available_worker = check_available_worker(workers, N);
         while(available_worker == -1) {
-            available_worker = check_available_worker(N);
+            available_worker = check_available_worker(workers, N);
         }
 
         // notify worker to process value and wait for it to start processing
         workers[available_worker].value_to_process = input;
         workers[available_worker].command = PROCESS_VALUE;
-        // perimenoume ton worker na ksekinhsei na doulevei wste na mhn ksanadwsei h main task ston idio worker
+        // wait for the worker to start working
         while (workers[available_worker].state != WORKING) {}
     }
 
     for (int i = 0; i < N; i++)
         workers[i].command = TERMINATE;
-    while(!check_terminated_workers(N)) {}
+    // wait for all workers to terminate
+    while(!check_terminated_workers(workers, N)) {}
 
     free(workers);
-    return 0;
-}
-
-int check_terminated_workers(const int N) {
-    for (int i = 0; i < N; i++) {
-        if (workers[i].state != TERMINATING)
-            return 0;
-    }
-    return 1;
-}
-
-int check_available_worker(const int N) {
-    for (int i = 0; i < N; i++) {
-        if (workers[i].state == AVAILABLE)
-            return i;
-    }
-    return -1;
-}
-
-int create_workers(const int N) {
-    workers = malloc(N * sizeof(worker_t));
-
-    for (int i = 0; i < N; i++) {
-        workers[i].id = i;
-        workers[i].state = AVAILABLE;
-        workers[i].value_to_process = 0;
-        workers[i].command = WAIT;
-        pthread_t thread;
-        pthread_create(&thread, NULL, run_worker, &workers[i]);
-    }
     return 0;
 }
 
@@ -101,13 +71,44 @@ void *run_worker(void *arg) {
         self->state = AVAILABLE;
         while (self->command == WAIT) {}
         if (self->command == TERMINATE) break;
-        //self acknowledging command
+        // reset command before setting the state of the worker
+        // so that the master doesn't get his commands overwritten by the worker
         self->command = WAIT;
         self->state = WORKING;
         printf("Worker #%d: %lld is %sprime\n", self->id, self->value_to_process, is_prime(self->value_to_process) ? "" : "not ");
     }
     self->state = TERMINATING;
     return NULL;
+}
+
+int check_terminated_workers(const worker_t *workers, const int N) {
+    for (int i = 0; i < N; i++) {
+        if (workers[i].state != TERMINATING)
+            return 0;
+    }
+    return 1;
+}
+
+int check_available_worker(const worker_t *workers, const int N) {
+    for (int i = 0; i < N; i++) {
+        if (workers[i].state == AVAILABLE)
+            return i;
+    }
+    return -1;
+}
+
+int create_workers(worker_t **workers, const int N) {
+    *workers = malloc(N * sizeof(worker_t));
+
+    for (int i = 0; i < N; i++) {
+        (*workers)[i].id = i;
+        (*workers)[i].state = AVAILABLE;
+        (*workers)[i].value_to_process = 0;
+        (*workers)[i].command = WAIT;
+        pthread_t thread;
+        pthread_create(&thread, NULL, run_worker, &(*workers)[i]);
+    }
+    return 0;
 }
 
 int is_prime(const long long int N) {
