c97aa2b43ad2fdcfcd61c26d9cd427017c68c000
nikas <nikasjason1@gmail.com>
2024-11-07 15:53:51 +0200

    Add ask2 working

--- Project2/assignment2/src/ask2.c
+++ Project2/assignment2/src/ask2.c
@@ -25,10 +25,10 @@ enum master_command {
 */
 typedef struct worker {
     int id;
-    mysem_t worker_queue;
-    mysem_t m_wait_available;
-    mysem_t w_wait_command;
-    bool waiting_for_command;
+    pthread_t thread;
+    mysem_t *worker_queue;
+    mysem_t *m_wait_available;
+    mysem_t *w_wait_command;
     enum master_command command;
     long long int value_to_process;
 } worker_t;
@@ -41,7 +41,7 @@ typedef struct worker {
     worker_t **workers - the pointer to the array of workers.
     int N - the size of the array to create.
 */
-void create_workers(worker_t **workers, const int N, const mysem_t worker_queue, const mysem_t m_wait_available, const mysem_t w_wait_command);
+void create_workers(worker_t **workers, const int N, mysem_t *worker_queue, mysem_t *m_wait_available, mysem_t *w_wait_command);
 
 /*
     The function to run on each worker thread.
@@ -56,18 +56,6 @@ void create_workers(worker_t **workers, const int N, const mysem_t worker_queue,
 void *run_worker(void *arg);
 
 /*
-    Finds an available worker in the given array of workers.
-
-    Parameters: 
-    worker_t *workers - the array of workers to search.
-    int N - the size of the array workers.
-    Returns:
-    -1, if there isn't an available worker in the array or
-    an integer that belongs to the id of the available worker found.
-*/
-int get_available_worker(const worker_t *workers, int N);
-
-/*
     Checks if a number is prime using
     a very naive approach.
 
@@ -78,6 +66,8 @@ int get_available_worker(const worker_t *workers, int N);
     0, if the number N is not prime.
 */
 int is_prime(long long int N);
+
+static worker_t *available_worker;
 
 int main(const int argc, char *argv[]) {
     if (argc != 2) {
@@ -93,7 +83,7 @@ int main(const int argc, char *argv[]) {
     mysem_init(&m_wait_available, 0);
     mysem_init(&worker_queue, 0);
     mysem_init(&w_wait_command, 0);
-    create_workers(&workers, N, worker_queue, m_wait_available, w_wait_command);
+    create_workers(&workers, N, &worker_queue, &m_wait_available, &w_wait_command);
 
     while (1) {
         const int read_result = scanf("%lld", &input);
@@ -101,21 +91,21 @@ int main(const int argc, char *argv[]) {
             break;
 
         // wait until you find an available worker
-        mysem_up(&worker_queue);
+        int ret = mysem_up(&worker_queue);
         mysem_down(&m_wait_available);
         // notify worker to process value and wait for it to start processing
-        int available_worker_id = get_available_worker(workers, N);
-        worker_t *available_worker = workers + available_worker_id;
         available_worker->value_to_process = input;
         available_worker->command = PROCESS_VALUE;
-        mysem_up(&w_wait_command);
+        ret = mysem_up(&w_wait_command);
     }
 
     for (int i = 0; i < N; i++) {
-        mysem_up(&worker_queue);
+        int ret = mysem_up(&worker_queue);
         mysem_down(&m_wait_available);
-        workers[i].command = TERMINATE;
-        mysem_up(&w_wait_command);
+        available_worker->command = TERMINATE;
+        ret = mysem_up(&w_wait_command);
+        //giati den doulevei xwris auto?
+        //pthread_join(available_worker->thread, NULL);
     }
 
     free(workers);
@@ -125,39 +115,30 @@ int main(const int argc, char *argv[]) {
 void *run_worker(void *arg) {
     worker_t *self = arg;
     while(1) {
-        self->waiting_for_command = false;
-        mysem_down(&self->worker_queue);
-        self->waiting_for_command = true;
-        mysem_up(&self->m_wait_available);
-        mysem_down(&self->w_wait_command);
+        mysem_down(self->worker_queue);
+        available_worker = self;
+        int ret = mysem_up(self->m_wait_available);
+        mysem_down(self->w_wait_command);
         
-        if (self->command == TERMINATE)
+        if (self->command == TERMINATE) {
+            printf("Worker #%d: terminating...\n", self->id);
             break;
+        }
         printf("Worker #%d: %lld is %sprime\n", self->id, self->value_to_process, is_prime(self->value_to_process) ? "" : "not ");
     }
     return NULL;
 }
 
-int get_available_worker(const worker_t *workers, const int N) {
-    for (int i = 0; i < N; i++) {
-        if (workers[i].waiting_for_command)
-            return i;
-    }
-    return -1;
-}
-
-void create_workers(worker_t **workers, const int N, const mysem_t worker_queue, const mysem_t m_wait_available, const mysem_t w_wait_command) {
+void create_workers(worker_t **workers, const int N, mysem_t *worker_queue, mysem_t *m_wait_available, mysem_t *w_wait_command) {
     *workers = malloc(N * sizeof(worker_t));
 
-    pthread_t thread;
     for (int i = 0; i < N; i++) {
         (*workers)[i].id = i;
         (*workers)[i].worker_queue = worker_queue;
         (*workers)[i].m_wait_available = m_wait_available;
         (*workers)[i].w_wait_command = w_wait_command;
         (*workers)[i].value_to_process = 0;
-        (*workers)[i].waiting_for_command = false;
-        const int res = pthread_create(&thread, NULL, run_worker, &(*workers)[i]);
+        const int res = pthread_create(&((*workers)[i].thread), NULL, run_worker, &(*workers)[i]);
         if (res)
             printf("Failed to create worker %d: %d", i, res);
     }
