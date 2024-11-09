8bfc80e387b7d736b2d3d1c0465404cff1e19aab
Tvangos <teov10092004@gmail.com>
2024-11-09 03:54:14 +0200

    Working version with extra mutex in passenger thread

--- Project2/assignment4/src/assignment4.c
+++ Project2/assignment4/src/assignment4.c
@@ -5,8 +5,6 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-#define SEAT_NUM 5
-
 void create_train();
 void create_passenger();
 void *train_thread(void *arg);
@@ -14,13 +12,15 @@ void *passenger_thread(void *arg);
 
 typedef struct thread_info {
     pthread_t ptid;
+    int max_passengers;
 } thread_info_t;
 
-int passenger_count = 0;
+int passengers_onboard = 0;
 int passengers_waiting = 0;
 mysem_t wait_train;
 mysem_t wait_passengers;
 mysem_t mtx;
+mysem_t mtx2;
 mysem_t ride;
 
 int main(int argc, char *argv[]) {
@@ -37,29 +37,30 @@ int main(int argc, char *argv[]) {
     mysem_init(&ride, 0);
 
     // add train thread
-    create_train();
+    create_train(N);
 
     while(1) {
         double sleep_time;
         scanf("%lf", &sleep_time);
         sleep(sleep_time);
-        for (int i=0; i<N; i++)
-            create_passenger();
+        create_passenger(N);
     }
     
     return 0;
 }
 
-void create_train() {
+void create_train(int max_passengers) {
     thread_info_t *info = (thread_info_t *) malloc(sizeof(thread_info_t));
+    info->max_passengers = max_passengers;
 
     int rc = pthread_create(&(info->ptid), NULL, train_thread, info);
     if (!rc) 
         printf("Successfully created train thread %ld\n", info->ptid);
 }
 
-void create_passenger() {
+void create_passenger(int max_passengers) {
     thread_info_t *info = (thread_info_t *) malloc(sizeof(thread_info_t));
+    info->max_passengers = max_passengers;
 
     int rc = pthread_create(&(info->ptid), NULL, passenger_thread, info);
     if (!rc) 
@@ -67,21 +68,23 @@ void create_passenger() {
 }
 
 void *train_thread(void *arg) {
-    
+    thread_info_t *info = (thread_info_t *) arg;
+    int max_passengers = info->max_passengers;
+
     while (1) {
 
         mysem_down(&mtx);
         // N or more passengers waiting
-        if (passengers_waiting >= SEAT_NUM) {
+        if (passengers_waiting >= max_passengers) {
             // unblock N of them
-            for (int i = 0; i < SEAT_NUM; i++) {
+            for (int i = 0; i < max_passengers; i++) {
                 passengers_waiting--;
                 printf("Train woke up passenger %d.\n", i);
                 mysem_up(&wait_train);
             }
         }
         // less than N passengers waiting
-        else if (passengers_waiting < SEAT_NUM && passengers_waiting > 0) {
+        else if (passengers_waiting < max_passengers && passengers_waiting > 0) {
             while (passengers_waiting > 0) {
                 passengers_waiting--;
                 printf("Train woke up a passenger.\n");
@@ -89,20 +92,21 @@ void *train_thread(void *arg) {
             }
         }
         mysem_up(&mtx);
-        // wait until train is full
         printf("Train waiting for passengers.\n");
+        // wait until train is full
         mysem_down(&wait_passengers);
 
         /***during this code no new passengers should get in train ***/
         // ride
         printf("Train will go for a ride.\n");
         sleep(5);
+        printf("Train finished ride.\n");
 
         // let all passengers get out
         mysem_down(&mtx);
-        for (int i=0; i < SEAT_NUM; i++) {
-            passenger_count--;
-            printf("Passenger %d left train.\n", i);
+        for (int i=0; i < max_passengers; i++) {
+            passengers_onboard--;
+            printf("Train told passenger %d to leave.\n", i);
             mysem_up(&ride);
         }
         mysem_up(&mtx);
@@ -115,30 +119,34 @@ void *train_thread(void *arg) {
 void *passenger_thread(void *arg) {
     thread_info_t *info = (thread_info_t *) arg;
     pthread_t ptid = info->ptid;
+    int max_passengers = info->max_passengers;
 
+    mysem_down(&mtx2);
     mysem_down(&mtx);
-    // train on a ride or currently emptying
-    if (passenger_count <= SEAT_NUM) {
+    // train on a ride 
+    if (passengers_onboard == max_passengers) {
         passengers_waiting++;
-        mysem_up(&mtx); // ama allaksei cpu kai kanei >2 up to treno xanei up
+        mysem_up(&mtx); // xwris mtx2 race condition edw 
         printf("Passenger %ld is waiting for train.\n", ptid); 
         mysem_down(&wait_train);
-    }
 
-    // mporei na adeiazei to treno kai na 
-    // erthei epibaths , ara tha kollhsei edw  
+        mysem_down(&mtx2);
+    }
+    else 
+        mysem_up(&mtx);
+
     mysem_down(&mtx);
     // get into train
-    passenger_count++;
-    if (passenger_count == SEAT_NUM) {
+    passengers_onboard++;
+    if (passengers_onboard == max_passengers) {
         // let train start
-        printf("N-th passenger %ld woke up train.\n", ptid);
+        printf("%d-th passenger %ld woke up train.\n", max_passengers, ptid);
         mysem_up(&wait_passengers);
     }
     mysem_up(&mtx);
 
     // enjoy ride
-    printf("Passenger %ld will go for a ride.\n", ptid);
+    printf("Passenger %ld is on board train.\n", ptid);
     mysem_down(&ride);
 
     return NULL;
