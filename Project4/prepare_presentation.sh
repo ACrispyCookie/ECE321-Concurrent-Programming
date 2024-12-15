c8506f17533e5aee8e6bcf1828ed25f48cf231d6
nikas <nikasjason1@gmail.com>
2024-12-15 03:16:00 +0200

    Add more functionality to prepare_presentation.sh

--- Project4/prepare_presentation.sh
+++ Project4/prepare_presentation.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-for i in {1..4}; do
+
+CLEAR=$1
+
+echo "Clearing binaries folders..."
+for i in {1..3}; do
   target_folder="assignment$i/target"
 
   if [ -d "$target_folder" ]; then
@@ -9,7 +13,7 @@ for i in {1..4}; do
     echo "$target_folder does not exist"
   fi
 done
-for i in {3..4}; do
+for i in {1..1}; do
   target_folder="project1_assignment$i/target"
 
   if [ -d "$target_folder" ]; then
@@ -19,13 +23,34 @@ for i in {3..4}; do
     echo "$target_folder does not exist"
   fi
 done
-for i in {3..4}; do
-  target_folder="project2_assignment$i/target"
 
-  if [ -d "$target_folder" ]; then
-    rm -rf "$target_folder"/*
-    echo "Emptied $target_folder"
-  else
-    echo "$target_folder does not exist"
-  fi
-done
+target_folder="list/target"
+
+if [ -d "$target_folder" ]; then
+  rm -rf "$target_folder"/*
+  echo "Emptied $target_folder"
+else
+  echo "$target_folder does not exist"
+fi
+
+if [ "$CLEAR" == "-c" ]; then
+  exit 1
+fi
+
+echo ""
+echo "Making list library..."
+cd list
+make
+
+echo ""
+echo "Making coroutine library..."
+cd ../assignment1
+make
+
+echo ""
+echo "Making threads library..."
+cd ../assignment2
+make 
+
+
+
